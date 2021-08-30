779adbc803cb1cb6c1c06c925bd49967c2351f10
jonasx10j <jonasx.dufek@gmail.com>
2021-08-30 22:51:24 +0200

    Initial commit

---
+++ src/main/java/Static_config/Tdlight_config.java
@@ -0,0 +1,10 @@
+package Static_config;
+
+public interface Tdlight_config {
+    String DATABASE_DIRECTORY = "tdlib";//No need to change
+    int APP_ID = 7643227; //Get ID, Hash at: https://my.telegram.org/apps
+    String API_HASH = "688267c52649d44d3e6a70d6c433b5df";
+    String SYSTEM_LANGUAGE_CODE = "en";
+    String DEVICE_MODEL = "desktop";//No need to change
+    String APP_VERSION = "1.0";//No need to change
+}
---
+++ src/main/java/Static_config/TelegramBot_config.java
@@ -0,0 +1,5 @@
+package Static_config;
+
+public interface TelegramBot_config {
+    String BOT_TOKEN = "<>"; //Get via bot father chat
+}
